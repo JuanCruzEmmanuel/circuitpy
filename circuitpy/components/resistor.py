0df7601923c2a1d119d781f028c2d38dd7a7364d
Juan Cruz Noya <juancruznoya@mi.unc.edu.ar>
2025-11-10 18:59:08 -0300

    Se modifican nombres de referencias incorrectas a la resistencia

--- circuitpy/components/resistor.py
+++ circuitpy/components/resistor.py
@@ -3,12 +3,24 @@
 from .base import * #Importo todo los elementos de base
 
 class Resistor(Component): #Importo las caracteristicas de Component
-    def __init__(self, name, n1, n2, resistance, rho=0, temp=20, pot = 0.25):
+    
+    
+    def __init__(self, name, n1, n2, resistance, alpha=0.0039, t0=20.0, pot_max = 0.25):
+        """
+        Modelado de resistencia
+        
+        -alpha: Coeficiente de temperatura en 1/ºC; si no me equivoco 0.0039 es la del cobre
+        -t0: Temperatura de referencia en ºC
+        -pot_max: potencia maxima que soporta la resistencia
+        
+        """
         super().__init__(name, n1, n2)
-        self.R = resistance #Si se agrega un valor a rho la resistencia debe modificarse en funcion a la temperatura
-        self.rho = rho
-        self.t0 = temp
-        self.pot = pot
+        self.R = resistance #Si se agrega un valor a alpha la resistencia debe modificarse en funcion a la temperatura
+        self.alpha = alpha
+        self.t0 = t0
+        self.Tt = t0 #temperatura en tiempo inicia igual que la que se estima "nominal"
+        self.pot_max = pot_max
+        self.burned = False #variable que va a controlar si el componente se quema, tal vez es mejor ponerle failure
         
     def stamp(self, G, I, node_map):
         g = 1.0/self.R #la conductancia es la inversa de la resistencia
