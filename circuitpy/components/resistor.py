1bd020a2cfdcd90e51c6767d0000963b375ffee4
Juan Cruz Noya <juancruznoya@mi.unc.edu.ar>
2025-11-10 20:01:48 -0300

    Se agrega una funcion que controla la temperatura de la resistencia

--- circuitpy/components/resistor.py
+++ circuitpy/components/resistor.py
@@ -15,12 +15,32 @@ class Resistor(Component): #Importo las caracteristicas de Component
         
         """
         super().__init__(name, n1, n2)
+        self.R0 = resistance
         self.R = resistance #Si se agrega un valor a alpha la resistencia debe modificarse en funcion a la temperatura
         self.alpha = alpha
         self.t0 = t0
         self.Tt = t0 #temperatura en tiempo inicia igual que la que se estima "nominal"
         self.pot_max = pot_max
         self.burned = False #variable que va a controlar si el componente se quema, tal vez es mejor ponerle failure
+        
+    def update_temperature(self,current=0.000001,t = 1.0):
+        """
+        funcion que actualiza la temperatura y la resistencia en funcion a la corriente que circula por el resistor
+        -current: Corriente que circula; lo inicio en un valor muy chico
+        -t: Tiempo de simulacion en segundos
+        """
+        #Pot = I2R
+        P = (current ** 2) * self.R
+        dT = P * t # E/T = Pot; E = P*T con E: Energia
+        #Supongo que voy a tener que poner info relacionada a la TJ, TA y RJA
+        self.Tt = self.t0 + dT
+        self.R = self.R0 * (1+ self.alpha * (self.Tt - self.t0))
+        
+        if P > self.pot_max:
+            self.burned = True
+            
+            print(f" {self.name} burned! Power={P:.2f}W > {self.pot_max}W")
+            print(f"Temperature= {self.Tt}")
         
     def stamp(self, G, I, node_map):
         g = 1.0/self.R #la conductancia es la inversa de la resistencia
